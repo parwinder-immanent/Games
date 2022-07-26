4230002d7d61a275b9fc12c29473ca07a221cf72
sharique001 <sharique.immanent@gmail.com>
2022-07-27 03:07:33 +0545

    day end

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import RegisterForm from './Components/RegisterForm';
+// import RegisterForm from './Components/RegisterForm';
 //import Register from './Components/Register';
-import Login from './Components/Login';
-import {BrowserRouter,Routes,Route,} from "react-router-dom";
+// import Login from './Components/Login';
+// import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import Catch from "./Components/Catch";
 
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<RegisterForm/>} />
-      <Route path="/Login" element={<Login/>} />
+     {/* <BrowserRouter> */}
+    {/* <Routes> */}
+      {/* <Route path="/" element={<RegisterForm/>} />
+      <Route path="/Login" element={<Login/>} /> */}
 
-    </Routes>
-  </BrowserRouter>
+    {/* </Routes> */}
+  {/* </BrowserRouter> */}
   
-    
+    <Catch/>
   
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -1,11 +1,77 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {
-  res.send('H')
-})
+//  just check
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+// Authenticate page 
+app.get('/api', (req, res) => {
+  res.json({
+ message:    'Welcome to the our authenticate  section  '
+  }
+  );
+});
+
+
+
+app.post('/api/user/authenticate', verifyToken, (req, res) => {
+  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403)
+    } else {
+      res.json({
+        message: 'post is  created....',
+        authData
+      });
+    }
+  });
+}); 
+
+app.post('/api/user/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: 'brad',
+    email: 'brad@gmail.com'
+  }
+  jwt.sign({ user }, 'secretkey', { expiresIn: '500s' }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+}); 
+
+
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  console.log(bearerHeader);
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
+app.listen(5000, () => console.log('Server started on port 5000')); 
+
+
+
+
+
